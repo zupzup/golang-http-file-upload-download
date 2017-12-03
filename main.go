8608f929a71419b47d1b8ca12bc0f9ab81934395
Mario Zupan <mzupanmz@gmail.com>
2017-12-03 21:22:11 +0100

    basic example

--- main.go
+++ main.go
@@ -1,7 +1,99 @@
 package main
 
-import "fmt"
+import (
+	"bytes"
+	"fmt"
+	"github.com/go-chi/chi"
+	"github.com/satori/go.uuid"
+	"io"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"path/filepath"
+)
+
+const maxUploadSize = 2 * 1024 * 2014 // 2 mb
+const uploadPath = "./tmp"
 
 func main() {
-	fmt.Println("file-upload-download")
+	r := chi.NewRouter()
+	r.Post("/upload", uploadFileHandler())
+	fileServer(r, "/files", http.Dir(uploadPath))
+
+	log.Print("Server started on localhost:8080, use /upload for uploading files and /files/{fileName} for downloading")
+	log.Fatal(http.ListenAndServe(":8080", r))
+}
+
+func uploadFileHandler() http.HandlerFunc {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// validate file size
+		r.Body = http.MaxBytesReader(w, r.Body, maxUploadSize)
+		if err := r.ParseMultipartForm(maxUploadSize); err != nil {
+			renderError(w, "FILE_TOO_BIG", http.StatusBadRequest)
+			return
+		}
+
+		// parse and validate file and post parameters
+		fileType := r.PostFormValue("type")
+		file, header, err := r.FormFile("uploadFile")
+		if err != nil {
+			renderError(w, "INVALID_FILE", http.StatusBadRequest)
+			return
+		}
+		defer file.Close()
+		originalFilename := header.Filename
+		fileBytes, err := ioutil.ReadAll(file)
+		if err != nil {
+			renderError(w, "INVALID_FILE", http.StatusBadRequest)
+			return
+		}
+
+		// check file type, detectcontenttype only needs the first 512 bytes
+		filetype := http.DetectContentType(fileBytes[:512])
+		if filetype != "image/jpeg" && filetype != "image/jpg" &&
+			filetype != "image/gif" && filetype != "image/png" &&
+			filetype != "application/pdf" {
+			w.Write([]byte("INVALID_FILE_TYPE"))
+			return
+		}
+		fileName := uuid.NewV4().String()
+		fileEnding := filepath.Ext(originalFilename)
+		newPath := fmt.Sprintf("%s/%s%s", uploadPath, fileName, fileEnding)
+		fmt.Printf("FileType: %s, File: %s\n", fileType, newPath)
+
+		// write file
+		newFile, err := os.Create(newPath)
+		if err != nil {
+			renderError(w, "CANT_WRITE_FILE", http.StatusBadRequest)
+			return
+		}
+		defer newFile.Close()
+		f := bytes.NewReader(fileBytes)
+		if _, err := io.Copy(newFile, f); err != nil {
+			renderError(w, "CANT_WRITE_FILE", http.StatusBadRequest)
+			return
+		}
+		w.Write([]byte("SUCCESS"))
+	})
+}
+
+func fileServer(r chi.Router, path string, root http.FileSystem) {
+	fs := http.StripPrefix(path, http.FileServer(root))
+
+	if path != "/" && path[len(path)-1] != '/' {
+		r.Get(path, http.RedirectHandler(path+"/", 301).ServeHTTP)
+		path += "/"
+	}
+	path += "*"
+
+	r.Get(path, http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fs.ServeHTTP(w, r)
+		return
+	}))
+}
+
+func renderError(w http.ResponseWriter, message string, statusCode int) {
+	w.WriteHeader(http.StatusBadRequest)
+	w.Write([]byte(message))
 }
